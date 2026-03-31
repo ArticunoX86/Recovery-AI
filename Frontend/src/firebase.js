22a903424fac56f75233beed664473c46b7948f5
ArticunoX86 <mprathamesh86@gmail.com>
2026-03-31 10:41:43 +0530

    Refactor firebase.js to remove duplicate code
    Removed redundant firebase configuration code.

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -1,18 +1,3 @@
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAGHyL6uFMLVm1j2IsZoXdOKnJ7VJsFg2g",
-  authDomain: "recoveryai-34279.firebaseapp.com",
-  projectId: "recoveryai-34279",
-  storageBucket: "recoveryai-34279.firebasestorage.app",
-  messagingSenderId: "614269917263",
-  appId: "1:614269917263:web:325cfadef278e4b912723a"
-};
-
-const app = initializeApp(firebaseConfig);
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
